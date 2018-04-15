accd7f1dd24f39b776dc7d1d17967594468456df
sonusinghrajput11 <sonusinghrajput@outlook.com>
2018-04-16 01:21:10 +0530

    Updated

--- Machine_Learning_Tensorflow_Examples/Logistic_Regression_Classification.py
+++ Machine_Learning_Tensorflow_Examples/Logistic_Regression_Classification.py
@@ -1,17 +1,8 @@
 
-# coding: utf-8
-
-# In[102]:
-
-
 import tensorflow as tf
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import scale
-
-
-# In[103]:
-
 
 #Reading iris dataset
 X_Actual, Y_Actual = load_iris(True)
@@ -27,24 +18,12 @@ Y_Train = tf.one_hot(Y_Actual[:135], 3)
 X_Test = scale(X_Actual[135:])
 Y_Test = tf.one_hot(Y_Actual[135:], 3)
 
-
-# In[104]:
-
-
 #Definig place holder to hold data
 X = tf.placeholder(tf.float32, shape = (None,4))
 Y = tf.placeholder(tf.float32, shape = (None,3))
 
-
-# In[105]:
-
-
 W = tf.Variable(tf.random_normal([4,3], stddev = 0.35), name = "weight")
 b = tf.Variable(tf.random_normal([3]), name = "bais")
-
-
-# In[106]:
-
 
 #Caluclating cross entropy
 Y_Prediction = tf.nn.softmax(tf.add(tf.matmul(X, W), b))
@@ -53,16 +32,8 @@ cross_entropy = -(Y * tf.log(Y_Prediction))
 loss = tf.reduce_mean(tf.reduce_sum(cross_entropy, axis=1))
 #tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=Y_Prediction)
 
-
-# In[141]:
-
-
 learning_rate = 0.05
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-
-
-# In[142]:
-
 
 no_of_epochs = 5000
 display_step = 1000
@@ -80,10 +51,6 @@ for epoch in range(no_of_epochs):
 print("***********************************Training Finished!******************************************")
 training_loss = sess.run(loss, feed_dict = {X: X_Train, Y: Y_Train.eval()})
 print("Training loss=", training_loss, "W=", sess.run(W), "b=", sess.run(b), '\n')
-
-
-# In[143]:
-
 
 #Model Validation and Testing
 test_loss = sess.run(loss, feed_dict = {X: X_Test, Y: Y_Test.eval()})
